feb054fb9fec37d16e0d21963617d0edf81ca826
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2021-08-24 19:05:07 -0500

    entradas 2

--- 0824/entradas.py
+++ 0824/entradas.py
@@ -1,7 +1,12 @@
 entero = int(input("Escribe un entero: "))
 flotante = float(input("Escribe un flotante: "))
-cadena = input("Escribe una cadena")
+cadena = input("Escribe una cadena: ")
 
 print("El entero es", entero)
 print("el flotante es", flotante)
 print("La cadena es " + cadena)
+
+
+print(type(entero))
+print(type(flotante))
+print(type(cadena))
