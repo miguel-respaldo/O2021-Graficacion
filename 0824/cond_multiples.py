ee1f7dd06d1285fd497cecc48b53ac56c6b8125b
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2021-08-24 19:22:29 -0500

    switch case

---
+++ 0824/cond_multiples.py
@@ -0,0 +1,12 @@
+mes = int(input("Escribe el número de un mes: "))
+
+if mes == 1:
+    print("Enero")
+elif mes == 2:
+    print("Febrero")
+elif mes == 3:
+    print("Marzo")
+elif mes == 4:
+    print("Abril")
+else:
+    print("El mes no existe")
