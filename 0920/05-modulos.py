d763b7f8434d49ce16861dc9d3cfe077360fbd75
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2021-09-20 18:25:48 -0500

    funciones y modulos 2

--- 0920/05-modulos.py
+++ 0920/05-modulos.py
@@ -1,21 +1,11 @@
-def suma(a,b):
-    resultado = a + b
-    return resultado
+import operaciones
 
-def resta(a,b):
-    resultado = a - b
-    return resultado
-
-def multiplicacion(a,b):
-    resultado = a * b
-    return resultado
-
-def division(a,b):
-    resultado = a / b
-    return resultado
+def princial():
+    print("La suma de 5 y 6 es ", operaciones.suma(5,6))
+    print("La resta de 5 y 6 es ", operaciones.resta(5,6))
+    print("La multiplicacion de 5 y 6 es ", operaciones.multiplicacion(5,6))
+    print("La division de 5 y 6 es ", operaciones.division(5,6))
 
 
-print("La suma de 5 y 6 es ", suma(5,6))
-print("La resta de 5 y 6 es ", resta(5,6))
-print("La multiplicacion de 5 y 6 es ", multiplicacion(5,6))
-print("La division de 5 y 6 es ", division(5,6))
+if __name__ == "__main__":
+    princial()
---
+++ 0920/operaciones.py
@@ -0,0 +1,18 @@
+def suma(a,b):
+    resultado = a + b
+    return resultado
+
+def resta(a,b):
+    resultado = a - b
+    return resultado
+
+def multiplicacion(a,b):
+    resultado = a * b
+    return resultado
+
+def division(a,b):
+    resultado = a / b
+    return resultado
+
+if __name__ == "__main__":
+    print("Estas son funciones basicas de matematicas")
