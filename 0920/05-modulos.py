3b51a6db7d62d78f07dd5f8ad72f8e7c6d0cc1b5
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2021-09-20 18:17:53 -0500

    funciones y modulos

---
+++ 0920/05-modulos.py
@@ -0,0 +1,21 @@
+def suma(a,b):
+    resultado = a + b
+    return resultado
+
+def resta(a,b):
+    resultado = a - b
+    return resultado
+
+def multiplicacion(a,b):
+    resultado = a * b
+    return resultado
+
+def division(a,b):
+    resultado = a / b
+    return resultado
+
+
+print("La suma de 5 y 6 es ", suma(5,6))
+print("La resta de 5 y 6 es ", resta(5,6))
+print("La multiplicacion de 5 y 6 es ", multiplicacion(5,6))
+print("La division de 5 y 6 es ", division(5,6))
