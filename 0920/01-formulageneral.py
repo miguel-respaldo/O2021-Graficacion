f55f224b76317222f1a6a592f0412f62a40dba4a
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2021-09-20 17:55:05 -0500

    Formula General

---
+++ 0920/01-formulageneral.py
@@ -0,0 +1,22 @@
+import math
+
+a = eval(input("Ingresa el valor de a: "))
+b = eval(input("Ingresa el valor de b: "))
+c = eval(input("Ingresa el valor de c: "))
+
+discriminante = b**2 - 4*a*c
+
+if discriminante < 0:
+    print("Las raices son imaginarias")
+    img=1j
+    x1 = -b / (2 * a) + (math.sqrt(-discriminante) / (2 * a))*img
+    x2 = -b / (2 * a) - (math.sqrt(-discriminante) / (2 * a))*img
+elif discriminante == 0:
+    print("Las raices son iguales")
+    x1 = x2 = -b / (2 * a)
+else:
+    x1 = (-b + math.sqrt(discriminante)) / (2 * a)
+    x2 = (-b - math.sqrt(discriminante)) / (2 * a)
+
+print("x1 = ", x1)
+print("x2 = ", x2)
