9afc116778e55a65c9dce4111f4bd788f273e1e5
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2021-09-20 18:11:26 -0500

    ejemplo while

---
+++ 0920/03-while.py
@@ -0,0 +1,9 @@
+print("Programa que imprime las tablas de multiplicar\n")
+numero = int(input("Escribe la tabla a imprimir: "))
+
+i=1
+while i<11:
+    print(i,"x",numero,"=",i*numero)
+    #i = i + 1
+    i += 1
+
